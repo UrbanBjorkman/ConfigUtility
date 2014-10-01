de74e4156688c512d3ff38e5257c419b44aa5bd3
UrbanBjorkman <urban.bjorkman@gmail.com>
2014-10-01 10:54:06 +0200

    Added solution

---
+++ ConfigUtility/ClearConfig.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace ConfigUtility
+{
+	public class ClearConfig : Task
+	{
+
+		private string _configFilename;
+		[Required]
+		public string ConfigFilename
+		{
+			get { return _configFilename; }
+			set { _configFilename = value; }
+		}
+
+		public override bool Execute()
+		{
+			try
+			{
+				DoExecute();
+			}
+			catch (Exception ex)
+			{
+				Log.LogErrorFromException(ex);
+				return false;
+			}
+			return true;
+		}
+
+		private void DoExecute()
+		{
+			XmlDocument xmlDoc = GenerateEmpty();
+
+			xmlDoc.Save(ConfigFilename);
+
+		}
+
+
+
+		private XmlDocument GenerateEmpty()
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
+			sb.AppendLine("  <appSettings>");
+
+			//foreach (var kvp in keyValues)
+			//{
+			//	sb.AppendLine(string.Format("    <add key=\"{0}\" value=\"{1}\" />", kvp.Key, kvp.Value));
+			//}
+
+			sb.AppendLine("  </appSettings>");
+
+			var outDocument = new XmlDocument();
+			outDocument.LoadXml(sb.ToString());
+			return outDocument;
+
+		}
+	}
+}
---
+++ ConfigUtility/MergeConfig.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace ConfigUtility
+{
+
+	public class MergeConfig : Task
+	{
+		private string _sourceConfigFilename;
+
+		[Required]
+		public string SourceConfigFilename
+		{
+			get { return _sourceConfigFilename; }
+			set { _sourceConfigFilename = value; }
+		}
+
+		private string _targetConfigFilename;
+
+		[Required]
+		public string TargetConfigFilename
+		{
+			get { return _targetConfigFilename; }
+			set { _targetConfigFilename = value; }
+		}
+
+
+		public override bool Execute()
+		{
+			try
+			{
+				DoExecute();
+			}
+			catch (Exception ex)
+			{
+				Log.LogErrorFromException(ex);
+				return false;
+			}
+			return true;
+		}
+
+		private void DoExecute()
+		{
+			var config = Load(SourceConfigFilename);
+
+			config = Merge(TargetConfigFilename, config);
+
+			Generate(config).Save(TargetConfigFilename);
+
+			Console.WriteLine("Completed merging XML documents");
+
+		}
+
+
+		private XmlDocument Generate(Dictionary<string, string> keyValues)
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
+			sb.AppendLine("  <appSettings>");
+
+			foreach (var kvp in keyValues)
+			{
+				sb.AppendLine(string.Format("    <add key=\"{0}\" value=\"{1}\" />", kvp.Key, kvp.Value));
+			}
+
+			sb.AppendLine("  </appSettings>");
+
+			var outDocument = new XmlDocument();
+			outDocument.LoadXml(sb.ToString());
+			return outDocument;
+
+		}
+
+
+		private Dictionary<string, string> Load(string file)
+		{
+			var configKeys = new Dictionary<string, string>();
+			var document = new XmlDocument();
+			if (!System.IO.File.Exists(SourceConfigFilename)) return configKeys;
+
+			document.Load(SourceConfigFilename);
+			var keys = document.SelectNodes("/appSettings/add");
+
+			if (keys != null)
+			{
+				configKeys =
+					keys.Cast<XmlNode>()
+						.Where(node => node.Attributes != null)
+						.ToDictionary(node => node.Attributes["key"].Value, node => node.Attributes["value"].Value);
+			}
+
+			return configKeys;
+		}
+
+		private Dictionary<string, string> Merge(string file, Dictionary<string, string> configKeys)
+		{
+			var document = new XmlDocument();
+
+			if (!System.IO.File.Exists(TargetConfigFilename)) return configKeys;
+
+			document.Load(TargetConfigFilename);
+			var keys = document.SelectNodes("/appSettings/add");
+			if (keys == null) return configKeys;
+			foreach (
+				var node in
+					keys.Cast<XmlNode>()
+						.Where(node => node.Attributes != null && !configKeys.ContainsKey(node.Attributes["key"].Value)))
+			{
+				configKeys.Add(node.Attributes["key"].Value, node.Attributes["value"].Value);
+			}
+			return configKeys;
+		}
+	}
+}
